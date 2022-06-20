553c417d0b1b5f51e4a2a0cf13d7a766f3288f06
Thiago Damasceno <tdeoliveira@questrade.com>
2022-06-20 17:50:51 -0300

    BLLs Prontas

---
+++ Modelo.ORM/BLL/CategoriaBll.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Modelo.ORM.INFO;
+using Modelo.ORM.DAL;
+
+namespace Modelo.ORM.Bll
+{ 
+
+    public class CategoriaBll
+    {
+        CategoriaDal dal = new CategoriaDal();
+
+        public CategoriaInfo Salvar(CategoriaInfo obj)
+        {
+            return dal.Salvar(obj);
+        }
+
+        public CategoriaInfo Selecionar(int categoriaid)
+        {
+            return dal.Selecionar(categoriaid);
+        }
+
+        public bool Excluir(int categoriaid)
+        {
+            return dal.Excluir(categoriaid);
+        }
+
+        public List<CategoriaInfo> SelecionarTodos()
+        {
+            return dal.SelecionarTodos();
+        }
+    }
+}
---
+++ Modelo.ORM/BLL/ProdutoBll.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Modelo.ORM.INFO;
+using Modelo.ORM.DAL;
+
+namespace Modelo.ORM.BLL
+{
+    public class ProdutoBll
+    {
+        ProdutoDal dal = new ProdutoDal();
+
+        public ProdutoInfo Salvar (ProdutoInfo obj)
+        {
+            return dal.Salvar(obj);
+        }
+
+        public ProdutoInfo Selecionar (int produtoid)
+        {
+            return dal.Selecionar(produtoid);
+        }
+
+        public bool Excluir (int categoriaid)
+        {
+            return dal.Excluir(categoriaid);
+        }
+
+        public List<ProdutoInfo> SelecionarTodos()
+        {
+            return dal.SelecionarTodos();
+        }
+    }
+}
